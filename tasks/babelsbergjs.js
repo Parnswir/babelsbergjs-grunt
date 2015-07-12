68435ca427ac955eddc65ed444c3b5f2091319ac
Maximilian Grundke <maximiliangrundke@googlemail.com>
2015-07-12 15:49:41 +0200

    Fix cwd

--- tasks/babelsbergjs.js
+++ tasks/babelsbergjs.js
@@ -16,13 +16,13 @@ module.exports = function(grunt) {
 
     this.files.forEach(function(f) {
       f.src.forEach(function(filepath) {
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
+        if (!grunt.file.exists(f.cwd + filepath)) {
+          grunt.log.warn('Source file "' + f.cwd + filepath + '" not found.');
           
         }
-        var content = grunt.file.read(filepath)
+        var content = grunt.file.read(f.cwd + filepath)
         var transformed = new BabelsbergSrcTransform().transform(content);
-        var filename = f.dest + filepath.replace(/.*\//, "").replace(".bbb", ".js");
+        var filename = f.dest + filepath.replace(".bbb", ".js");
         grunt.file.write(filename, transformed);
       });
     });
