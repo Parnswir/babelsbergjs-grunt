99151ac5812d4e3f292563a643ab106efe130ca1
Maximilian Grundke <maximiliangrundke@googlemail.com>
2015-07-12 15:40:48 +0200

    Fix task

--- tasks/babelsbergjs.js
+++ tasks/babelsbergjs.js
@@ -8,21 +8,23 @@
 
 'use strict';
 
-var BabelsbergSrcTransform = require("node_modules/babelsbergjs-require/src/src_transform");
+var BabelsbergSrcTransform = require("./../node_modules/babelsbergjs-require/src/src_transform.js");
 
 module.exports = function(grunt) {
 
   grunt.registerMultiTask('babelsbergjs', 'Helper for BabelsbergJS for Node.', function() {
 
     this.files.forEach(function(f) {
-      if (!grunt.file.exists(filepath)) {
-        grunt.log.warn('Source file "' + filepath + '" not found.');
-        
-      }
-      var content = grunt.file.read(filepath)
-      var transformed = new BabelsbergSrcTransform().transform(content);
-      var filename = file.dest + path.basename(f, '.bbb') + '.js'
-      grunt.file.write(filename, transformed);
+      f.src.forEach(function(filepath) {
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          
+        }
+        var content = grunt.file.read(filepath)
+        var transformed = new BabelsbergSrcTransform().transform(content);
+        var filename = f.dest + filepath.replace(/.*\//, "").replace(".bbb", ".js");
+        grunt.file.write(filename, transformed);
+      });
     });
   
   });
